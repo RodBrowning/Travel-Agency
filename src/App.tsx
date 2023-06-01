9e4d789d10649efb4905f35fa8494a27b765eac3
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2023-06-01 19:31:06 -0300

    Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import './scss/fonts/Cardo/stylesheet.scss'
 import './scss/index.scss'
 
 import Header from './components/header/index'
+import Hero from './components/hero'
 import React from 'react'
 
 const App: React.FC = () => {
    return (
       <div className="App bg-primary">
          <Header />
+         <Hero />
          {/* main */}
          {/* footer */}
       </div>
---
+++ src/components/hero/index.tsx
@@ -0,0 +1,25 @@
+import './style.scss'
+import './small-devices.scss'
+
+import React from 'react'
+
+const Hero: React.FC = () => {
+   return (
+      <section id="hero" className="container py-2">
+         <div className="text">
+            <h1 className="title-big mb-2">Travel to your dream destination</h1>
+            <p className="mb-4">
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+               vero, numquam velit maiores consectetur sapiente maxime ad
+               aspernatur odio.
+            </p>
+            <button className="btn-secondary">Reserve Ticket</button>
+         </div>
+         <div className="heroImage">
+            <img src="/assets/heroImage.png" alt="Hero image" />
+         </div>
+      </section>
+   )
+}
+
+export default Hero
