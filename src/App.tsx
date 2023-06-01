da6065db2b8f6947fc2c07d54d69ab95c11bdb29
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2023-06-01 15:48:48 -0300

    Setup fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import './App.scss'
+import './scss/fonts/Lato/stylesheet.scss'
+import './scss/fonts/Cardo/stylesheet.scss'
+import './scss/index.scss'
 
 import React from 'react'
 
 const App = () => {
    return (
       <div className="App">
-         <header className="App-header"></header>
+         <h1>Lato</h1>
       </div>
    )
 }
