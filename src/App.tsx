dbe146e73d1ae0bcda22d318fb4e29265f5448c7
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2023-06-01 16:39:34 -0300

    Setup colors

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import React from 'react'
 
 const App = () => {
    return (
-      <div className="App">
-         <h1 className="title-big">Lorem</h1>
-         <h2 className="title-medium">Lorem</h2>
+      <div className="App bg-primary">
+         <h1 className="title-big bg-secondary">Lorem</h1>
+         <h2 className="title-medium bg-tertiary">Lorem</h2>
          <h3 className="title-small">Lorem</h3>
          <p className="regular-text-title">Lorem</p>
          <p>Lorem</p>
